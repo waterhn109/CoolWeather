f52386f85bdd438020c0d9d52e6fa8f7a1dc0425
waterhn109 <waterhn109@126.com>
2016-08-19 16:43:38 +0800

    20160819

--- app/src/main/java/com/coolweather/app/util/GetXmldata.java
+++ app/src/main/java/com/coolweather/app/util/GetXmldata.java
@@ -1,91 +1,57 @@
 package com.coolweather.app.util;
 
-import android.util.Log;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.StringReader;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * Created by HY on 2016/8/18.
  */
 public class GetXmldata {
 
-    private static List<Xmldata>  Xmldatas;
+    private static List<Xmldata> Xmldatas;
+
+    public static String substr(String str1,String str) {
+
+
+
+        //String str = "<p>sad2f</p>";//两个字符串中间
+        String regex = str1+"=\""+"(.[^\"]*.)"+"\"";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);//匹配类
+        while (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
+    }
 
     public static List<Xmldata> GetXmldata(String response){
 
-        try {
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            XmlPullParser xmlPullParser = factory.newPullParser();
-            xmlPullParser.setInput(new StringReader(response));
-            int eventType = xmlPullParser.getEventType();
-            String id = "";
-            String name = "";
-            String version = "";
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                Xmldata xmldata = new Xmldata();
-                xmldata.setUpcity(XMLRootSearcher.findRootElementName(response));
-                String nodeName = xmlPullParser.getName();
-                switch (eventType) {
-                    // 开始解析某个结点
-                    case XmlPullParser.START_TAG: {
-                        if ("cityX".equals(nodeName)) {
-                            xmldata.setCityX(xmlPullParser.nextText());
-                        } else if ("cityY".equals(nodeName)) {
-                            xmldata.setCityY(xmlPullParser.nextText());
-                        } else if ("cityname".equals(nodeName)) {
-                            xmldata.setCityname(xmlPullParser.nextText());
-                        } else if ("centername".equals(nodeName)) {
-                            xmldata.setCentername(xmlPullParser.nextText());
-                        } else if ("pyName".equals(nodeName)) {
-                            xmldata.setPyName(xmlPullParser.nextText());
-                        } else if ("stateDetailed".equals(nodeName)) {
-                            xmldata.setStateDetailed(xmlPullParser.nextText());
-                        } else if ("tem1".equals(nodeName)) {
-                            xmldata.setTem1(xmlPullParser.nextText());
-                        } else if ("tem2".equals(nodeName)) {
-                            xmldata.setTem2(xmlPullParser.nextText());
-                        } else if ("temNow".equals(nodeName)) {
-                            xmldata.setTemNow(xmlPullParser.nextText());
-                        } else if ("windState".equals(nodeName)) {
-                            xmldata.setWindState(xmlPullParser.nextText());
-                        } else if ("windDir".equals(nodeName)) {
-                            xmldata.setWindDir(xmlPullParser.nextText());
-                        } else if ("windPower".equals(nodeName)) {
-                            xmldata.setWindPower(xmlPullParser.nextText());
-                        } else if ("humidity".equals(nodeName)) {
-                            xmldata.setHumidity(xmlPullParser.nextText());
-                        } else if ("url".equals(nodeName)) {
-                            xmldata.setUrl(xmlPullParser.nextText());
-                        }
-                        else if ("quName".equals(nodeName)) {
-                            xmldata.setQuName(xmlPullParser.nextText());
-                        }
-                        else if ("time".equals(nodeName)) {
-                            xmldata.setTime(xmlPullParser.nextText());
-                        }
-                        break;
-                    }
-                    // 完成解析某个结点
-//                    case XmlPullParser.END_TAG: {
-//                        if ("app".equals(nodeName)) {
-//                            Log.d("MainActivity", "id is " + id);
-//                            Log.d("MainActivity", "name is " + name);
-//                            Log.d("MainActivity", "version is " + version);
-//                        }
-//                        break;
-//                    }
-                    default:
-                        break;
-                }
-                Xmldatas.add(xmldata);
-                eventType = xmlPullParser.next();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        String reg_charset = "/>";
+        String[] strs = response.split(reg_charset);
+        for (int i = 0; i < strs.length-1; i++)
+        {
+            Xmldata xmldata = new Xmldata();
+            xmldata.setQuName(substr("quName",strs[i]));
+            xmldata.setPyName(substr("pyName",strs[i]));
+            xmldata.setCityname(substr("cityname",strs[i]));
+            xmldata.setStateDetailed(substr("stateDetailed",strs[i]));
+            xmldata.setTem1(substr("tem1",strs[i]));
+            xmldata.setTem2(substr("tem2",strs[i]));
+            xmldata.setWindState(substr("windState",strs[i]));
+
+            xmldata.setTemNow(substr("temNow",strs[i]));
+            xmldata.setWindDir(substr("windDir",strs[i]));
+            xmldata.setWindPower(substr("windPower",strs[i]));
+            xmldata.setHumidity(substr("humidity",strs[i]));
+            xmldata.setTime(substr("time",strs[i]));
+            xmldata.setCityY(substr("cityY",strs[i]));
+            xmldata.setCityX(substr("cityX",strs[i]));
+            xmldata.setCentername(substr("centername",strs[i]));
+
+            Xmldatas.add(xmldata);
+
         }
         return Xmldatas;
     }
--- app/src/main/java/com/coolweather/app/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/app/util/HttpUtil.java
@@ -1,5 +1,12 @@
 package com.coolweather.app.util;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,21 +23,31 @@ public class HttpUtil {
 			public void run() {
 				HttpURLConnection connection = null;
 				try {
-					URL url = new URL(address);
-					connection = (HttpURLConnection) url.openConnection();
-					connection.setRequestMethod("GET");
-					connection.setConnectTimeout(8000);
-					connection.setReadTimeout(8000);
-					InputStream in = connection.getInputStream();
-					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-					StringBuilder response = new StringBuilder();
-					String line;
-					while ((line = reader.readLine()) != null) {
-						response.append(line);
+					HttpClient httpClient = new DefaultHttpClient();
+					HttpGet httpGet = new HttpGet(address);
+					HttpResponse httpResponse = httpClient.execute(httpGet);
+					String response = "" ;
+					if(httpResponse.getStatusLine().getStatusCode() ==200){
+						//请求和响应都成功
+						HttpEntity entity = httpResponse.getEntity();
+						 response = EntityUtils.toString(entity,"utf-8");
+
 					}
+//					URL url = new URL(address);
+//					connection = (HttpURLConnection) url.openConnection();
+//					connection.setRequestMethod("GET");
+//					connection.setConnectTimeout(8000);
+//					connection.setReadTimeout(8000);
+//					InputStream in = connection.getInputStream();
+//					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+//					StringBuilder response = new StringBuilder();
+//					String line;
+//					while ((line = reader.readLine()) != null) {
+//						response.append(line);
+//					}
 					if (listener != null) {
 						// 回调onFinish()方法
-						listener.onFinish(response.toString());
+						listener.onFinish(response);
 					}
 				} catch (Exception e) {
 					if (listener != null) {
